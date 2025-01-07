3b498db72de7dcf1c244439f1fb04c7f74ead6aa
shota-nagato <shota.nagato@gmail.com>
2025-01-07 20:39:44 +0900

    setup datebase & product display

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,12 +2,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Card, CardHeader, CardContent } from '@/components/ui/card'
 import ProductPrice from './product-price'
+import { Product } from '@/types'
 
-const ProductCard = ({ product }: { product: any }) => {
+const ProductCard = ({ product }: { product: Product }) => {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="w-full max-w-sm">
-        <Link href={`/products/${product.slug}`}>
+        <Link href={`/product/${product.slug}`}>
           <Image
             src={product.images[0]}
             alt={product.name}
@@ -17,7 +18,7 @@ const ProductCard = ({ product }: { product: any }) => {
         </Link>
         <CardContent className="p-4 grid gap-4">
           <div className="text-sx">{product.brand}</div>
-          <Link href={`/products/${product.slug}`}>
+          <Link href={`/product/${product.slug}`}>
             <h2 className="text-sm font-medium">{product.name}</h2>
           </Link>
           <div className="flex-between gap-4">
